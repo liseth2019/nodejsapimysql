ba3e462f5469892852654eef220755036df48734
liseth <lsanchez@mapsalud.com>
2023-03-03 09:12:02 -0500

    change index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,8 @@
 import {pool} from '../db.js'
 
 export const ping = async (req, res) => {
-    const [result] = await pool.query("SELECT * FROM users")
+    //const [result] = await pool.query("SELECT * FROM users")
 
-    res.json(result)
+    res.json({message:'pong'})
 }
 
